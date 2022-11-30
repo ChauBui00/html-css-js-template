3dcf56e570b231ff1d5967acf9389d44bbe8841f
ChauBui00 <c.bui3@share.epsb.ca>
2022-11-30 10:50:23 -0700

    Update main.js

--- main.js
+++ main.js
@@ -31,23 +31,41 @@
 // console.log("Heads: " + numHeads + " Tails: " + numTails);
 
  // Coin Flip Simulator
-      let numHeads = 0;
-      let numTails = 0
-      let numFlips = 0;
+    //   let numHeads = 0;
+    //   let numTails = 0
+    //   let numFlips = 0;
 
-      // Loop until 100 heads
-      while (numHeads < 100) {
-        // Simulate Coin Flip 50/50 chance
-        if (Math.random() < 0.5) {
-          numHeads++;
-        } else {
-          numTails++;
-        }
-        // Increment numFlips
-        numFlips++;
-      }
+    //   // Loop until 100 heads
+    //   while (numHeads < 100) {
+    //     // Simulate Coin Flip 50/50 chance
+    //     if (Math.random() < 0.5) {
+    //       numHeads++;
+    //     } else {
+    //       numTails++;
+    //     }
+    //     // Increment numFlips
+    //     numFlips++;
+    //   }
   
-      // Output Final Results
-      console.log("Number of Coin Flips: " + numFlips);
-      console.log("Heads: " + numHeads + " Tails: " + numTails);
-      
+    //   // Output Final Results
+    //   console.log("Number of Coin Flips: " + numFlips);
+    //   console.log("Heads: " + numHeads + " Tails: " + numTails);
+
+
+// Ask user for  a number b/t 1 and 10
+// let num = +prompt("Enter a number b/t 1 and 10:");
+
+// while( !(num >= 1 && num <= 10)) {
+//     num = +prompt("Sorry I need a number b/t 1 and 10:");
+
+// }
+// alert('Thank you for the number!');
+
+// Loop through the rows
+for (let y = 2; y <= 10; y += 2){
+
+// Print out a row 
+for (let x = 2; x <= 8;  x += 2) {
+    console.log(" (" + x + " , "+ y +") ");
+    }
+}
